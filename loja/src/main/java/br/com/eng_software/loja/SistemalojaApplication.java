8b4a27d44b352017cab48ebf3328bda488f10630
Paulo Vitor <80797702+paulovitor21@users.noreply.github.com>
2022-03-17 23:12:37 -0400

    Projeto criado

---
+++ loja/src/main/java/br/com/eng_software/loja/SistemalojaApplication.java
@@ -0,0 +1,13 @@
+package br.com.eng_software.loja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemalojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemalojaApplication.class, args);
+	}
+
+}
---
+++ loja/src/test/java/br/com/eng_software/loja/SistemalojaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eng_software.loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemalojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
